0949055c837dcb357aea03c40f7776e50fb9be95
DanielDimitrov5 <danieldimitrov2304@gmail.com>
2023-12-21 12:04:08 +0200

    Nonce & double wrap check

--- src/Bridge.sol
+++ src/Bridge.sol
@@ -8,7 +8,6 @@ import {ECDSA} from "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import {MessageHashUtils} from "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
 import {console2} from "forge-std/Console2.sol";
 
-
 contract Bridge is Ownable {
     using ECDSA for bytes32;
     using MessageHashUtils for bytes32;
@@ -18,9 +17,12 @@ contract Bridge is Ownable {
     error Bridge__InvalidAmount(uint256 amount);
     error Bridge__InvalidSignature();
     error Bridge__TokenAlreadyWrapped(address token);
+    error Bridge__InvalidNonce(uint256 nonce);
+    error Bridge__InvalidRecepient(address recepient);
 
     address public immutable ADMIN;
-    mapping (address originalToken => WERC20 wrappedToken) public wrappedTokens;
+    mapping(address originalToken => WERC20 wrappedToken) public wrappedTokens;
+    mapping(address user => uint256 nonce) public nonces;
 
     constructor(address admin) Ownable(msg.sender) {
         ADMIN = admin;
@@ -30,7 +32,14 @@ contract Bridge is Ownable {
         if (token == address(0)) revert Bridge__InvalidToken();
         if (amount == 0) revert Bridge__InvalidAmount(amount);
         if (chainId == 0) revert Bridge__InvalidChainId(chainId);
-        _;        
+        _;
+    }
+
+    modifier validateMint(address token, uint256 amount, address to) {
+        if (token == address(0)) revert Bridge__InvalidToken();
+        if (amount == 0) revert Bridge__InvalidAmount(amount);
+        if (to == address(0)) revert Bridge__InvalidRecepient(to);
+        _;
     }
 
     event Lock(address indexed token, address indexed sender, uint256 amount, uint256 indexed chainId);
@@ -42,25 +51,39 @@ contract Bridge is Ownable {
         emit Lock(token, msg.sender, amount, chainId);
     }
 
-    function mint(address originalToken, uint256 amount, address to, bytes memory signature) external {
-        bytes32 messageHash = keccak256(abi.encodePacked(originalToken, to, amount, block.chainid));
+    function mint(address originalToken, uint256 amount, address to, uint256 nonce, bytes memory signature)
+        external
+        validateMint(originalToken, amount, to)
+    {
+        if (nonces[to] != nonce) {
+            revert Bridge__InvalidNonce(nonce);
+        }
+
+        bytes32 messageHash = keccak256(abi.encodePacked(originalToken, to, amount, block.chainid, nonce));
         bytes32 ethSignedMessageHash = messageHash.toEthSignedMessageHash();
-        
+
         address signer = ethSignedMessageHash.recover(signature);
         if (signer != ADMIN) {
             revert Bridge__InvalidSignature();
         }
 
+        nonces[to]++;
+
         WERC20 wrappedToken = wrappedTokens[originalToken];
         if (address(wrappedToken) == address(0)) {
-            string memory name = string(abi.encodePacked("Wrapped", ERC20(originalToken).name()));
-            string memory symbol = string(abi.encodePacked("w", ERC20(originalToken).symbol()));
-            wrappedToken = new WERC20(name, symbol);
-            wrappedTokens[originalToken] = wrappedToken;
-            emit TokenWrapped(originalToken, address(wrappedToken));
+            _wrapToken(originalToken);
+            wrappedToken = wrappedTokens[originalToken];
         }
 
         wrappedToken.mint(to, amount);
         emit Mint(address(wrappedToken), to, amount);
+    }
+
+    function _wrapToken(address token) internal {
+            string memory name = string(abi.encodePacked("Wrapped", ERC20(token).name()));
+            string memory symbol = string(abi.encodePacked("w", ERC20(token).symbol()));
+            WERC20 wrappedToken = new WERC20(name, symbol);
+            wrappedTokens[token] = wrappedToken;
+            emit TokenWrapped(token, address(wrappedToken));
     }
 }
